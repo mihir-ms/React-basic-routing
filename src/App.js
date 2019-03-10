c8f0047927aa38feffd41cac9cf42f95b106ece6
Mihir Shah <mihir-ms>
2019-03-10 11:58:42 +0530

    Menu Component Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import Header from './components/Header/header';
-import Home from './components/Home/home';
-import Login from './components/Login/login';
-import Carousel from './components/Carousel/carousel';
-import Notfound from './components/Not Found/notfound';
-import Routes from './routes';
+import React, { Component } from 'react';
+import { Navbar } from 'reactstrap';
+import { NavbarBrand } from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent/MenuComponent';
 
 class App extends Component {
   render() {
     return (
-      
-     <div><Header/><Carousel/><h1>App Js</h1><Home/><Login/><Notfound/></div>
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">MS Restaurent</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu/>
+      </div>
     );
   }
 }
